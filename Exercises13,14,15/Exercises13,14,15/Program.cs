4b90ee03b2f2af49f5f3e14e035f49b2b80b956e
siddharth-I <67158060+siddharth-I@users.noreply.github.com>
2021-08-11 17:58:07 -0400

    Add files via upload

---
+++ Exercises13,14,15/Exercises13,14,15/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercises13_14_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //Exercise 13
+        {
+            Console.WriteLine("Enter a number: ");
+            int numberOne = int.Parse(Console.ReadLine());
+            for (int i = numberOne; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            // Exercise 14
+            Console.WriteLine("Enter a number: ");
+            int numberTwo = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= numberTwo; i++)
+            {
+                Console.WriteLine(i * i);
+            }
+
+            //Exercise 15
+            Console.WriteLine("Enter a number: ");
+            int numberThree = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= numberThree; i++)
+            {
+                Console.WriteLine(i * i * i);
+            }
+        }
+    }
+}
